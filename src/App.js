d3c7540cf9851dd2b0655fa350c4d77f0217b463
vikaschowdary47 <vikaschowdary47@gmail.com>
2020-07-18 19:11:49 +0530

    random advice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import './App.css'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [advice, setAdvice] = useState('')
+
+    useEffect(() => {
+        fetchAdvice()
+    }, [])
+
+    function fetchAdvice() {
+        axios
+            .get('https://api.adviceslip.com/advice')
+            .then(response => {
+                const { advice } = response.data.slip;
+                setAdvice(advice)
+                console.log(advice)
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+
+    return (
+        <div className="app">
+            <div className="main">
+                <div className="card">
+                    <h1 className="head">{advice}</h1>
+                    <button className="button" onClick={fetchAdvice}><span>Give me a free advice!</span></button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
